c21f6547ebe754bf3537cb2026d0f3209321c762
Shantanu Gagare <shantanugagare12@gmail.com>
2024-05-08 11:03:44 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -3,13 +3,13 @@ class Solution:
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
 
-     if not grid or not grid[0]:
+    if not grid or not grid[0]:
         return 0
     
     def dfs(i, j):
         if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
             return
-        grid[i][j] = 'W' 
+        grid[i][j] = 'W'  # Mark the land as visited by turning it into water
         # Explore the four possible directions
         dfs(i + 1, j)
         dfs(i - 1, j)
