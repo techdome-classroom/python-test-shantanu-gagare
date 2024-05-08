aff7e48c1d01c94272660be0362aceb0bb30e5ac
Shantanu Gagare <shantanugagare12@gmail.com>
2024-05-08 11:19:00 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -8,7 +8,7 @@ class Solution:
             
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
                 return
-            grid[i][j] = 'W'  # Mark the land as visited by turning it into water
+            grid[i][j] = 'W'
 
             dfs(i + 1, j)
             dfs(i - 1, j)
