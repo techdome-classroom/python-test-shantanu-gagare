b906b6335fae6c29c66ccf10d168b44d4bad46aa
Shantanu Gagare <shantanugagare12@gmail.com>
2024-05-08 11:14:29 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,6 +2,8 @@ class Solution:
    
     # def getTotalIsles(self, grid: list[list[str]]) -> int:
     # #    write your code here
+                if not grid or not grid[0]:
+            return 0
 
     def numIslands(grid):
         if not grid or not grid[0]:
