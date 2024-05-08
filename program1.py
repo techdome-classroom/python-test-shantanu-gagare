e0eae3f58d66debeb4a71d71fd2af3a1c65ff03a
Shantanu Gagare <shantanugagare12@gmail.com>
2024-05-08 11:27:51 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -7,6 +7,8 @@ class Solution:
         def search_island(i, j):
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
                 return
+            
+            
             grid[i][j] = 'W'
             search_island(i + 1, j)
             search_island(i - 1, j)
