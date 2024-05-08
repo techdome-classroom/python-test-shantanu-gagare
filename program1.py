6a38ad6018722b5e3394446a77e4b47785fbf0cd
Shantanu Gagare <shantanugagare12@gmail.com>
2024-05-08 11:09:19 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -5,21 +5,21 @@ class Solution:
         if not grid or not grid[0]:
             return 0
         
-        def search(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
-                return
-            grid[i][j] = 'W'  
-            search(i + 1, j)
-            search(i - 1, j)
-            search(i, j + 1)
-            search(i, j - 1)
-        
-        island_count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 'L':
-                    island_count += 1
-                    search(i, j)
+    def search(i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
+            return
+        grid[i][j] = 'W'  
+        search(i + 1, j)
+        search(i - 1, j)
+        search(i, j + 1)
+        search(i, j - 1)
+    
+    island_count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 'L':
+                island_count += 1
+                search(i, j)
         
         return island_count
     
