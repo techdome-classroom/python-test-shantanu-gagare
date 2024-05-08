83791f366dd005d4049eb71e3bc9fb9aff041539
Shantanu Gagare <shantanugagare12@gmail.com>
2024-05-08 11:19:59 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,23 +4,23 @@ class Solution:
         if not grid or not grid[0]:
             return 0
 
-        def dfs(i, j):
+        def search(i, j):
             
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
                 return
             grid[i][j] = 'W'
 
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
+            search(i + 1, j)
+            search(i - 1, j)
+            search(i, j + 1)
+            search(i, j - 1)
 
         island_count = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 'L':
                     island_count += 1
-                    dfs(i, j)
+                    search(i, j)
 
         return island_count
 
